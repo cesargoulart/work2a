568d6fb3601c0313ed979bd81cdce869df1b0839
cesargoulart <57975128+cesargoulart@users.noreply.github.com>
2024-12-27 01:29:30 +0000

    Update Flutter project dependencies and add new plugins

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_auth
 import firebase_core
 import firebase_database
+import path_provider_foundation
+import screen_retriever_macos
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
--- windows/flutter/standard_settings.cmake
+++ windows/flutter/standard_settings.cmake
@@ -1,6 +1,9 @@
 # standard_settings.cmake
 function(apply_standard_settings TARGET)
   target_compile_features(${TARGET} PUBLIC cxx_std_17)
+  set_target_properties(${TARGET} PROPERTIES
+    CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /std:c++17"
+  )
   target_compile_options(${TARGET} PRIVATE /W4 /WX /wd"4100")
   target_compile_options(${TARGET} PRIVATE /EHsc)
   target_compile_definitions(${TARGET} PRIVATE "_HAS_EXCEPTIONS=1")
